0fd72e03b7c1cd49706d957e26ee7af42197cc7c
omar1z <omarzammar11@gmail.com>
2024-07-15 20:41:22 +0300

    adding ingredients to the recipe having a name and the amount

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,36 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe('A test Recipe 1','this is simply a tes',
-          'https://th.bing.com/th/id/OIP.0wcq29kbowlHd3IKEzejSgHaLH?rs=1&pid=ImgDetMain'),
+          'https://th.bing.com/th/id/OIP.0wcq29kbowlHd3IKEzejSgHaLH?rs=1&pid=ImgDetMain',
+        [
+            new Ingredient('burger', 2),
+            new Ingredient('fries', 20)
+        ]
+        ),
         new Recipe('A test Recipe 2','this is simply a tes',
-            'https://th.bing.com/th/id/OIP.0wcq29kbowlHd3IKEzejSgHaLH?rs=1&pid=ImgDetMain'),
+            'https://th.bing.com/th/id/OIP.0wcq29kbowlHd3IKEzejSgHaLH?rs=1&pid=ImgDetMain',
+            [
+                new Ingredient('burger', 2),
+                new Ingredient('fries', 20)
+            ]),
         new Recipe('A test Recipe 3','this is simply a tes',
-              'https://th.bing.com/th/id/OIP.0wcq29kbowlHd3IKEzejSgHaLH?rs=1&pid=ImgDetMain'),
+              'https://th.bing.com/th/id/OIP.0wcq29kbowlHd3IKEzejSgHaLH?rs=1&pid=ImgDetMain',
+              [
+                  new Ingredient('burger', 2),
+                  new Ingredient('fries', 20)
+              ]),
         new Recipe('A test Recipe 4','this is simply a tes',
-                'https://th.bing.com/th/id/OIP.0wcq29kbowlHd3IKEzejSgHaLH?rs=1&pid=ImgDetMain')
+                'https://th.bing.com/th/id/OIP.0wcq29kbowlHd3IKEzejSgHaLH?rs=1&pid=ImgDetMain',
+                [
+                    new Ingredient('burger', 2),
+                    new Ingredient('fries', 20)
+                ])
       ];
 
     getRecipes() {
